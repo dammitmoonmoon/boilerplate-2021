697de35b8cdb783e8af7df1c19a1638fe017bd99
Yulia Modestova <jmodestova@yahoo.com>
2021-04-08 18:34:14 +0300

    build(root): Restrict commitlint config

--- config/git/commitlint.config.js
+++ config/git/commitlint.config.js
@@ -1,17 +1,20 @@
+//
+
 module.exports = {
   rules: {
-    "subject-empty": [2, "never"],
-    "type-empty": [2, "never"],
-
     "header-max-length": [2, "always", 72],
+
+    "subject-empty": [2, "never"],
+    "subject-case": [2, "always", "sentence-case"],
     "subject-full-stop": [2, "never", "."],
 
     "body-leading-blank": [2, "always"],
     "footer-leading-blank": [2, "always"],
 
     "scope-case": [2, "always", "lower-case"],
-    "type-case": [2, "always", "lower-case"],
 
+    "type-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
     "type-enum": [
       2,
       "always",
